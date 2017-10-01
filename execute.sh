84fa146019dfe8178c8697d9d822a1b58fbdbbdb
Shannon Code <genecyber@gmail.com>
2017-10-01 14:11:52 -0400

    exec

--- execute.sh
+++ execute.sh
@@ -1,7 +1,9 @@
 docker-compose -f runicorn-compose.yml up&
 source test.sh
 echo "done"
-open http://127.0.0.1:8080
-open http://127.0.0.1:8080/test/
+open -na "Google Chrome" --args --incognito http://127.0.0.1:8080/test &>/dev/null
+google-chrome --incognito http://127.0.0.1:8080/test& &>/dev/null
+open -na "Google Chrome" --args --incognito http://127.0.0.1:8080/target/site/surefire-report.html &>/dev/null
+google-chrome --incognito http://127.0.0.1:8080/target/site/surefire-report.html& &>/dev/null
 docker-compose -f runicorn-compose.yml down
 
