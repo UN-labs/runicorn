fa9ed22eae1e2758b8ffd6e775e3feae47f9b633
Shannon Code <genecyber@gmail.com>
2017-10-01 19:45:35 -0400

    new at framwork

--- execute.sh
+++ execute.sh
@@ -1,11 +1,18 @@
 docker-compose -f runicorn-compose.yml up&
-source test.sh
+sudo npm install wdio-json-reporter -g
+sudo npm install wdio-spec-reporter -g
+sudo npm install wdio-mocha-framework -g
+sudo npm install wdio-allure-reporter -g
+wdio test/at/wdio.conf.js
+allure generate test/at/report/ --clean
+#source test.sh
 echo "done"
 docker-compose -f runicorn-compose.yml down
 open -na "Google Chrome" --args --incognito http://127.0.0.1:3000/test &>/dev/null
 google-chrome --incognito http://127.0.0.1:3000/test& &>/dev/null
-open -na "Google Chrome" --args --incognito http://127.0.0.1:3000/target/site/surefire-report.html &>/dev/null
-google-chrome --incognito http://127.0.0.1:3000/target/site/surefire-report.html& &>/dev/null
+open -na "Google Chrome" --args --incognito http://127.0.0.1:3000/allure-report &>/dev/null
+google-chrome --incognito http://127.0.0.1:3000/allure-report& &>/dev/null
 npm install
 npm start
+rm -rf allure-report/
 
