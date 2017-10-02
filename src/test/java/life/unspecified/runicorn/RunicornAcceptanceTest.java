b615fa35c885ee32d0d9fcb2305d7f98a075c7cd
Unspecified Developer <31668871+unspecifieddeveloper@users.noreply.github.com>
2017-10-02 13:25:49 -0400

    update pom dependencies

--- src/test/java/life/unspecified/runicorn/RunicornAcceptanceTest.java
+++ src/test/java/life/unspecified/runicorn/RunicornAcceptanceTest.java
@@ -10,7 +10,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
